b88a39fdbdcf73a1b3ff1b0890de7a6168e79d74
RVS <alaskatoti@gmail.com>
2025-01-02 16:56:01 +0100

    dialogic

---
+++ mtfb/game_sccene_.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Dialogic.signal_event.connect(_on_dialogic_signal)
+	Dialogic.start("timeline")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_dialogic_signal(argument: String):
+	if argument == "its signal":
+		print("a signal war")
