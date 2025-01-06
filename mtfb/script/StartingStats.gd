b846c1caf8fc29f75f4a0ee2eb83fa192594f2db
RVS <alaskatoti@gmail.com>
2025-01-06 16:31:07 +0100

    Stats update

---
+++ mtfb/script/StartingStats.gd
@@ -0,0 +1,9 @@
+class_name Stats
+
+extends Resource
+
+@export var Survival: int = 10
+@export var Boobs: int = 1
+@export var Ass: int = 1 
+@export var Eyes: int = 1
+@export var Skinnyness: int = 1
---
+++ mtfb/script/subject_1.gd
@@ -0,0 +1,8 @@
+class_name Subj1
+extends Resource
+
+
+var stats : Stats = null 
+
+func load_stats (subj_stats: Stats) -> void:
+	stats = subj_stats 
